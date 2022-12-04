612bd0167021748d919dc109f171f925c5c53210
AlexDomanski <alex.domanskyi@gmail.com>
2022-12-04 19:02:28 +0100

    Импортированы модули react-router-dom

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,9 +1,34 @@
 import Main from '../../pages/main/main';
-
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import NotFound from '../../pages/not-found/not-found';
+import Login from '../../pages/login/login';
+import Property from '../../pages/property/property';
 
 function App(props: { offersAmount: number }): JSX.Element {
   return (
-    <Main offersAmount={props.offersAmount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={'/'}
+          element={<Main offersAmount={props.offersAmount} />}
+        />
+        <Route
+          path={'*'}
+          element={<NotFound />}
+        />
+        <Route
+          path={'/login'}
+          element={<Login />}
+        />
+        <Route
+          path={'/offer/:id'}
+          element={<Property />}
+        />
+      </Routes>
+
+
+    </BrowserRouter>
+
   );
 }
 
